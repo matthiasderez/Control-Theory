137a23ce2a4ccea45c4f634de6748f190446f81c
Matthias Derez <matthias.derez@student.kuleuven.be>
2021-12-22 17:56:42 +0100

    verslag

--- Assignment 4/Arduino/CT-EKF-Pendulum/extended_kalman_filter.cpp
+++ Assignment 4/Arduino/CT-EKF-Pendulum/extended_kalman_filter.cpp
@@ -6,42 +6,42 @@ void PredictionUpdate(const Matrix<1> &u, Matrix<3> &xhat, Matrix<3,3> &Phat) {
    const float L = 0.1259;      //Pendulum length [m]
    const float c = 0;      //Damping coefficient [Nm/s]
    const float g = 9.81;   //Gravitational acceleration [m/s^2]
-  
+   const float Ts = 0.01; 
    // Tuning parameter
-   float arrayQ[3][3]{ { 2.7878e-8,  0,  0},    //Provide here the element values of weight Q
-                       { 0,  9.4124e-8,  0},
-                       { 0,  0,  1e-4}};
+   float arrayQ[3][3]{ { 2.7878e-9,  0,  0},    //Provide here the element values of weight Q
+                       { 0,  9.4124e-9,  0},
+                       { 0,  0,  1e-5}};
   
    Matrix<3, 3> Q = arrayQ;
   
 
-  // Linear Kalman filter
-  float arrayF[3][3]{{1,0,0},
-                      {0,1,0.01/L},
-                      {0,-0.01*g,1}};
-  Matrix <3,3> F = arrayF;
-
-  float arrayG [3][1]{{0.01},{-0.01/L},{0}};
-  Matrix <3,1> G = arrayG;
-  xhat = F * xhat + G*u;
-  Phat = F*Phat*F.Transpose() + Q;
-
+//  // Linear Kalman filter
+//  float arrayF[3][3]{{1,0,0},
+//                      {0,1,0.01/L},
+//                      {0,-0.01*g,1}};
+//  Matrix <3,3> F = arrayF;
+//
+//  float arrayG [3][1]{{0.01},{-0.01/L},{0}};
+//  Matrix <3,1> G = arrayG;
+//  xhat = F * xhat + G*u;
+//  Phat = F*Phat*F.Transpose() + Q;
+//
 
   // Extended kalman filter
-  // // Compute Jacobian of system dynamics
-  // float arrayJf[3][3]{{?, ?, ?},   //Provide here the element values of the Jacobian of system dynamics
-  //                     {?, ?, ?},
-  //                     {?, ?, ?}};
-  // Matrix<3, 3> A = arrayJf;
-  //
-  // // Evaluate discrete-time nonlinear system dynamics
-  // float arrayf[3][1]{{ ? }, //Provide the nonlinear dynamics equation for each state
-  //                    { ? },
-  //                    { ? }};
-  // xhat = arrayf;    //state prediction is equal to the nonlinear dynamics calculated in arrayf
-  //
-  // // Update state covariance: P = APAt + Q, with A equal to the Jacobian of system dynamics
-  // Phat = A * Phat * A.Transpose() + Q;
+   // Compute Jacobian of system dynamics
+   float arrayJf[3][3]{{1, 0, 0},   //Provide here the element values of the Jacobian of system dynamics
+                       {0, 1+Ts*u(0)*sin(xhat(1))/L, Ts/L},
+                       {0,  Ts*(-g*cos(xhat(1)) - (xhat(2)-u(0)*cos(xhat(1)))/L*u(0)*cos(xhat(1))-pow(u(0)*sin(xhat(1)),2)/L), 1-Ts*u(0)*sin(xhat(1))/L}};
+   Matrix<3, 3> A = arrayJf;
+  
+   // Evaluate discrete-time nonlinear system dynamics
+   float arrayf[3][1]{{ Ts*u(0)+xhat(0) }, //Provide the nonlinear dynamics equation for each state
+                      { Ts*(xhat(2)-u(0)*cos(xhat(1)))/L + xhat(1) },
+                      { Ts*(-g*sin(xhat(1)) - u(0)*sin(xhat(1))*(xhat(2)-u(0)*cos(xhat(1)))/L)+xhat(2) }};
+   xhat = arrayf;    //state prediction is equal to the nonlinear dynamics calculated in arrayf
+  
+   // Update state covariance: P = APAt + Q, with A equal to the Jacobian of system dynamics
+   Phat = A * Phat * A.Transpose() + Q;
 }
 
 void CorrectionUpdate(const Matrix<2> &y, Matrix<3> &xhat, Matrix<3,3> &Phat, Matrix<2> &nu, Matrix<2,2> &S) {
@@ -51,7 +51,7 @@ void CorrectionUpdate(const Matrix<2> &y, Matrix<3> &xhat, Matrix<3,3> &Phat, Ma
                       {0, 9.4124e-6}};
    Matrix<2, 2> R = arrayR;
   // System C-matrix - Compute Jacobian of measurement equation
-   float arrayJh[2][3]{{0, 0, 0}, {0, 1, 0}}; //Provide here the element values of the Jacobian of measurement equation
+   float arrayJh[2][3]{{1, 0, 0}, {0, 1, 0}}; //Provide here the element values of the Jacobian of measurement equation
                        
    Matrix<2,3> C = arrayJh;
   
