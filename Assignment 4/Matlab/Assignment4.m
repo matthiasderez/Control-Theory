7ee4244e1f5af7088665a67405cffd05a8b94720
Toon Servaes <toon.servaes@student.kuleuven.be>
2021-12-23 16:44:28 +0100

    update

--- Assignment 4/Matlab/Assignment4.m
+++ Assignment 4/Matlab/Assignment4.m
@@ -62,22 +62,50 @@ figure()
 hold on 
 plotstates(obj, 1, 0.95)
 plotmeasurements(obj, 1, 0.95)
+xlabel('time [s]')
+ylabel('x [m]')
 hold off
 print -depsc state1.eps
+
+figure()
+hold on 
+plotstates(obj, 1, 0.95)
+plotmeasurements(obj, 1, 0.95)
+xlabel('time [s]')
+ylabel('x [m]')
+xlim([31.82 31.85])
+ylim([0.1947 0.19485])
+hold off
+print -depsc state1_closeup.eps
 
 %%% state 2
 figure()
 hold on 
 plotstates(obj, 2, 0.95)
 plotmeasurements(obj, 2, 0.95)
+xlabel('time [s]')
+ylabel('\theta [rad]')
 ylim([-0.6 0.6])
 hold off
 print -depsc state2.eps
+
+figure()
+hold on 
+plotstates(obj, 2, 0.95)
+plotmeasurements(obj, 2, 0.95)
+xlabel('time [s]')
+ylabel('\theta [rad]')
+xlim([35.5 37])
+ylim([-0.12 0.12])
+hold off
+print -depsc state2_closeup.eps
 
 %%% state 3
 figure()
 hold on 
 plotstates(obj, 3, 0.95)
+xlabel('time [s]')
+ylabel('v_{tan} [m/s]')
 hold off
 print -depsc state3.eps
 
@@ -85,6 +113,60 @@ print -depsc state3.eps
 
 
 % without wheel encoder
+R_theta = 9.4124e-6;
+
+R = [R_theta, 0; 0, 0];
+obj = KalmanExperiment.createfromQRC45();
+
+%%% state 1
+figure()
+hold on 
+plotstates(obj, 1, 0.95)
+xlabel('time [s]')
+ylabel('x [m]')
+hold off
+print -depsc state1_w.eps
+
+figure()
+hold on 
+plotstates(obj, 1, 0.95)
+xlabel('time [s]')
+ylabel('x [m]')
+xlim([131 138])
+ylim([-0.03 0.03])
+hold off
+print -depsc state1_w_closeup.eps
+
+%%% state 2
+figure()
+hold on 
+plotstates(obj, 2, 0.95)
+plotmeasurements(obj, 2, 0.95)
+xlabel('time [s]')
+ylabel('\theta [rad]')
+ylim([-0.6 0.6])
+hold off
+print -depsc state2_w.eps
+
+figure()
+hold on 
+plotstates(obj, 2, 0.95)
+plotmeasurements(obj, 2, 0.95)
+xlabel('time [s]')
+ylabel('\theta [rad]')
+xlim([133.7 135.1])
+ylim([-0.15 0.15])
+hold off
+print -depsc state2_w_closeup.eps
+
+%%% state 3
+figure()
+hold on 
+plotstates(obj, 3, 0.95)
+xlabel('time [s]')
+ylabel('v_{tan} [m/s]')
+hold off
+print -depsc state3_w.eps
 
 
 
